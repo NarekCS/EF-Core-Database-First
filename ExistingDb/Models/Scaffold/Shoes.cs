a9f7c9d9120269216431c45993ad13672dffdce3
narek.avanesyan <narek.avanesyan@FC-PROG-16>
2018-10-01 16:34:09 +0400

    Database First

---
+++ ExistingDb/Models/Scaffold/Shoes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExistingDb.Models.Scaffold
+{
+    public partial class Shoes
+    {
+        public Shoes()
+        {
+            ShoeCategoryJunction = new HashSet<ShoeCategoryJunction>();
+        }
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long ColorId { get; set; }
+        public decimal Price { get; set; }
+
+        public Colors Color { get; set; }
+        public SalesCampaigns SalesCampaigns { get; set; }
+        public ICollection<ShoeCategoryJunction> ShoeCategoryJunction { get; set; }
+    }
+}
